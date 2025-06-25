33ee596c011a1b458a40ef7352d1f88d16e4f3dd
xyugen <alex.arias.jorym@gmail.com>
2025-06-25 13:08:42 +0800

    feat: setup shadcn

--- src/app/layout/header.tsx
+++ src/app/layout/header.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="text-center spacey-2">
-      <h1 className="text-5xl font-semibold bg-gradient-to-b from-white to-slate-300 bg-clip-text text-transparent">
+    <header className="text-center space-y-2">
+      <h1 className="text-5xl font-semibold bg-gradient-to-b from-black to-slate-600 dark:from-white dark:to-slate-300 bg-clip-text text-transparent">
         Sonawave
       </h1>
-      <p>
+      <p className="text-slate-800 dark:text-slate-300">
         Converts <i>.wav</i> files into <b>16KHz, 16-bit, mono</b> format.
       </p>
     </header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="p-4">
       <Header />
-      <main></main>
+      <main>
+        
+      </main>
     </div>
   );
 }
